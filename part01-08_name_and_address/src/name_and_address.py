90308cc6cb1bf1b95276eff12eaaaeb922323a10
Md Zihad Hasan Jaan <mdzihadhasanjaan@gmail.com>
2024-11-02 02:04:05 +0200

    8 and 9 done

--- part01-08_name_and_address/src/name_and_address.py
+++ part01-08_name_and_address/src/name_and_address.py
@@ -1,1 +1,8 @@
-# Write your solution here
+
+gname = input("Given name:")
+fname = input("Family name:")
+saddress = input("Street address:")
+capcode = input("City and postal code:")
+print(gname, fname)
+print(saddress)
+print(capcode)
