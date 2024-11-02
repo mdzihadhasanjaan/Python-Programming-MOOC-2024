3db465050a381215e9b5a162a92b1b4ac9244de5
Md Zihad Hasan Jaan <mdzihadhasanjaan@gmail.com>
2024-11-02 02:49:15 +0200

    10b done

--- part01-10b_extra_space/src/extra_space.py
+++ part01-10b_extra_space/src/extra_space.py
@@ -10,9 +10,9 @@ level3 = "semiprofessional"
 lower = 2000
 upper = 3000
 
-print("my name is ", name, " , I am ", age, "years old")
-print("my skills are")
-print("- ", skill1, " (", level1, ")")
-print("- ", skill2, " (", level2, ")")
-print("- ", skill3, " (", level3, " )")
-print("I am looking for a job with a salary of", lower, "-", upper, "euros per month")
+print(f"my name is {name}, I am {age} years old")
+print("\nmy skills are")
+print(f" - {skill1} ({level1})")
+print(f" - {skill2} ({level2})")
+print(f" - {skill3} ({level3})")
+print(f"\nI am looking for a job with a salary of {lower}-{upper} euros per month")
