44ce08c804b1cec894dd8a1fb07133dfb980ddf2
Md Zihad Hasan Jaan <mdzihadhasanjaan@gmail.com>
2024-12-14 07:29:16 +0200

    16 done

--- part01-17_sum_and_product/src/sum_and_product.py
+++ part01-17_sum_and_product/src/sum_and_product.py
@@ -1,1 +1,10 @@
 # Write your solution here
+number1 = int(input("Number 1: "))
+number2 = int(input("Number 2: "))
+sum = number1 + number2
+product = number1 * number2
+print(f"The sum of the numbers: {sum}  The product of the numbers: {product}")
+
+
+
+
